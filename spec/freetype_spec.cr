c60157c42648e3353e0f02a33c6ffa289a43876a
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2022-04-23 23:47:39 +0200

    Clean up

--- spec/freetype_spec.cr
+++ spec/freetype_spec.cr
@@ -1,7 +1,27 @@
 require "./spec_helper"
-require "./freetype/*"
 
 describe Freetype do
-  # TODO: Write tests
+  describe "low level" do
+    describe "load char" do
+      it "should work" do
+        with_font_path do |path|
+          library = Freetype.get_handle
+          face = Pointer(LibFreetype::FT_Face).malloc
+          error = LibFreetype.FT_New_Face(library, path, 0, face)
+          raise "Failed to load font at path '#{path}'" if error > 0
+          error = LibFreetype.FT_Set_Char_Size(face.value, 0, 16 * 64, 300, 300)
+          raise "set char size failed" if error > 0
 
+          face = face.value
+
+          error = LibFreetype.FT_Load_Char(face, 'A'.ord, LibFreetype::FT_LOAD_RENDER)
+          raise "load char failed" if error > 0
+          bitmap = face.value.glyph.value.bitmap
+          width = face.value.glyph.value.bitmap.width
+          # puts "rows: #{bitmap.rows}, width: #{bitmap.width}, index: #{face.value.glyph.value}"
+          width.positive?.should be_true
+        end
+      end
+    end
+  end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,18 @@
 require "spec"
 require "../src/freetype"
+
+def with_face
+  with_font_path do |path|
+    puts "loading #{path}"
+    face = Freetype::Face.new(path)
+    face.set_char_size(0, 16 * 64, 300, 300)
+
+    yield face
+  end
+end
+
+def with_font_path
+  patterns = ["Ubuntu-R.ttf", "Vera.ttf"].map { |filename| "/usr/share/fonts/*/" + filename }
+  path = Dir.glob(patterns).first.to_s
+  yield path
+end
