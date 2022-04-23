b844885e08dc87883993f127711251193cb9586e
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2022-04-23 15:52:41 +0200

    Show more output in test to help with debugging

--- spec/freetype/face_spec.cr
+++ spec/freetype/face_spec.cr
@@ -13,6 +13,7 @@ describe "Freetype::Face" do
     it "puts a valid char bitmap in the glyph slot" do
       with_face do |subject|
         subject.load_char('A', LibFreetype::FT_LOAD_RENDER)
+        puts "rows: #{subject.glyph_slot.bitmap.rows}, width: #{subject.glyph_slot.bitmap.width}, index: #{subject.glyph_slot.glyph_slot.value.glyph_index}"
         subject.glyph_slot.bitmap.width.positive?.should be_true
       end
     end
